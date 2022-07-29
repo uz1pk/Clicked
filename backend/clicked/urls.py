ad5b8bba3a104adf2ebf8f1dee1fc47938760ca4
uz1pk <usmanozaheer@gmail.com>
2022-07-28 21:19:59 -0400

    resestting

--- backend/clicked/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.views.generic import TemplateView
-
-app_name = 'clicked'
-
-urlpatterns = [
-    path('', TemplateView.as_view(template_name="clicked/index.html"))
-]
--- backend/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('clicked.urls', namespace='clicked')),
-    path('', include('clicked_api.urls', namespace='clicked_api'))
-]
